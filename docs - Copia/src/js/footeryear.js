83f0da395bceb1086a9c7583e5c06e563462e716
Ruan <72177472+2nxty@users.noreply.github.com>
2024-08-31 17:39:59 -0300

    2

--- docs - Copia/src/js/footeryear.js
+++
@@ -1,9 +0,0 @@
-const yearText = document.querySelector(".footer__text-year")
-
-const setYear = () => {
-    const date = new Date();
-
-    yearText.textContent = date.getFullYear();
-}
-
-setYear()
--- docs/src/js/balance.js
+++ docs/src/js/balance.js
@@ -1,11 +1,7 @@
-
 const balanceAmount = document.querySelector(".nav__list-item-balance-amount");
 const depositInput = document.querySelector("#amount");
 const depositButton = document.querySelector(".main-dep__middle-deposit");
 const balanceAmountMobile = document.querySelector(".nav__balance-amount");
-const nigga = "999999"
-const leoreis = "6000"
-const q500 = "500"
 
 balanceAmount.textContent = localStorage.getItem("Balance");
 balanceAmountMobile.textContent = localStorage.getItem("Balance")
@@ -21,35 +17,4 @@ const addBalance = () => {
 	depositInput.value = ""
 };
 
-const addBalance2 = () => {
-	if (balanceAmount.textContent == "LEOREIS"){
-		let value = amountValue + leoreis;
-		balanceAmount.textContent = value.toFixed(2);
-		balanceAmountMobile.textContent = value.toFixed(2);
-		localStorage.setItem("Balance", `${balanceAmount.textContent}`);
-		depositInput.value = ""
-	}
-	else if (balanceAmount.textContent == "500conto"){
-		let value = amountValue + q500;
-		balanceAmount.textContent = value.toFixed(2);
-		balanceAmountMobile.textContent = value.toFixed(2);
-		localStorage.setItem("Balance", `${balanceAmount.textContent}`);
-		depositInput.value = ""
-	}
-	else if (balanceAmount.textContent == "nigga"){
-		let value = amountValue + nigga;
-		balanceAmount.textContent = value.toFixed(2);
-		balanceAmountMobile.textContent = value.toFixed(2);
-		localStorage.setItem("Balance", `${balanceAmount.textContent}`);
-		depositInput.value = ""
-	}
-	else{
-		let value = amountValue;
-		balanceAmount.textContent = value.toFixed(2);
-		balanceAmountMobile.textContent = value.toFixed(2);
-		localStorage.setItem("Balance", `${balanceAmount.textContent}`);
-		depositInput.value = ""
-	}
-};
-
-depositButton.addEventListener("click", addBalance2);
+depositButton.addEventListener("click", addBalance);
